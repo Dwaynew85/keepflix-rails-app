6a3cac37564b5dfc0902d94ba2f3dd28261662f8
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-09-29 02:59:50 -0400

    updated user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,18 @@ class UsersController < ApplicationController
         
     end
 
-    def create
+    def new
+        @users = User.new
+    end
 
+    def create
+        @users = User.new
+        @user.email = params[:user][:email]
+        if @user.save
+            redirect_to root_path
+        else
+            render 'users/new'
+        end
     end
 
     def edit
