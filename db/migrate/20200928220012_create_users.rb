8b55acd424929354d03216a94d31de3d00a1e83f
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-09-29 03:06:30 -0400

    added pic url to users migration

--- db/migrate/20200928220012_create_users.rb
+++ db/migrate/20200928220012_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :pic_url
       t.string :password_digest
 
       t.timestamps
