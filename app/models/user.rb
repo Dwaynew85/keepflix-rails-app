6a0b64d3e58595ae974a97bf28e8571579caac57
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-09-30 02:41:51 -0400

    added name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: {message: "Email required"}
+    validates :name, :email, presence: {message: "Email required"}
     validates :email, uniqueness: {message: "User email in use. Please log in with email or use another."}
 
     def self.find_or_create_by_omniauth(auth_hash)
