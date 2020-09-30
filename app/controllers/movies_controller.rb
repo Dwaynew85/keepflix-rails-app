b2483f87d4cfbc6cbde480abd7e24cc3abc907c6
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-09-30 03:32:47 -0400

    updated actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,28 @@
 class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def show 
+        @movie = Movie.find(params[:id])
+    end
+
+    def create
+        @movie = Movie.create_from_link(params[:movie][:link])
+
+        redirect_to movie_path(@movie)
+    end
+
+    def edit 
+        @movie = Movie.find(params[:id])
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        byebug
+    end
 end
