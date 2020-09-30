b7dd5116046b69ba86eb3f049d4032fd52686dc1
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-09-30 01:14:53 -0400

    added movies migration

---
+++ db/migrate/20200930044728_create_movies.rb
@@ -0,0 +1,22 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :stars
+      t.string :trailer_link
+      t.string :genres # change to model for sorting
+      t.string :rating
+      t.string :runtime
+      t.string :release_date
+      t.text :summary
+      t.text :image_url
+      t.text :link
+      t.string :opinion
+      t.text :comments # own model
+      t.datetime :post_time # comment model
+      t.integer :user_id # comment model too?
+
+      t.timestamps null: false
+    end
+  end
+end
