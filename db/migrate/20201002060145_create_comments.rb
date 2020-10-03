68810a1ac984f111afeaf839e8a5c77f02ae8483
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-10-03 01:45:56 -0400

    updated comment migration to join table

--- db/migrate/20201002060145_create_comments.rb
+++ db/migrate/20201002060145_create_comments.rb
@@ -2,7 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.string :content
-      t.integer :music_id
+      t.integer :movie_id
+      t.integer :user_id
 
       t.timestamps
     end
