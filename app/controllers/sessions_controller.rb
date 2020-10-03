29f71cda81faace2306b9ef6d5b9ed6e8db87dd9
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-10-03 01:43:33 -0400

    updated create action redirect to user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
             if user && user.authenticate(params[:password])
                 session[:user_id] = user.id
 
-                redirect_to root_path, notice: "Welcome #{user.name}!" 
+                redirect_to user_path(user), notice: "Welcome #{user.name}!" 
             else 
                 flash.new[:alert] = "Email, Name, or Password is invalid"
                 render 'new' #'sessions/new'
