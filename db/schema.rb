d13ae1a866c8459f264a2f049b294d0fe6189e15
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-10-03 21:19:34 -0400

    rendered form

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_02_060145) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "movie_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "stars"
+    t.string "trailer_link"
+    t.string "genres"
+    t.string "rating"
+    t.string "runtime"
+    t.string "release_date"
+    t.text "summary"
+    t.text "image_url"
+    t.text "link"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "pic_url"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: "Dwayne", email: "dwayne@me.com", password: "pw")
-User.create(name: "Joey", email: "joey@me.com", password: "pw")
-User.create(name: "Asia", email: "asia@me.com", password: "pw")
-User.create(name: "Dorie", email: "dorie@me.com", password: "pw")
+dwayne = User.create(name: "Dwayne", email: "dwayne@me.com", password: "pw")
+joey = User.create(name: "Joey", email: "joey@me.com", password: "pw")
+asia = User.create(name: "Asia", email: "asia@me.com", password: "pw")
+dorie = User.create(name: "Dorie", email: "dorie@me.com", password: "pw")
+
+dwayne.movies.create_from_link("https://www.imdb.com/title/tt4154796/?ref_=vp_back")
+dwayne.movies.create_from_link("https://www.imdb.com/title/tt1825683/?ref_=tt_sims_tt")
+dwayne.movies.create_from_link("https://www.imdb.com/title/tt7286456/?ref_=tt_sims_tt")
+dwayne.movies.create_from_link("https://www.imdb.com/title/tt0468569/?ref_=tt_sims_tt")
+
+joey.movies.create_from_link("https://www.imdb.com/title/tt8946378/?ref_=tt_sims_tt")
+joey.movies.create_from_link("https://www.imdb.com/title/tt1375666/?ref_=tt_sims_tt")
+joey.movies.create_from_link("https://www.imdb.com/title/tt0172495/?ref_=tt_sims_tt")
+
+asia.movies.create_from_link("https://www.imdb.com/title/tt0110912/?ref_=tt_sims_tt")
+asia.movies.create_from_link("https://www.imdb.com/title/tt0111161/?ref_=tt_sims_tt")
+
+dorie.movies.create_from_link("https://www.imdb.com/title/tt0109830/?ref_=tt_sims_tt")
+dorie.movies.create_from_link("https://www.imdb.com/title/tt0120689/?ref_=tt_sims_tt")
+dorie.movies.create_from_link("https://www.imdb.com/title/tt0102926/?ref_=tt_sims_tt")
+
+joker = Movie.all[2]
+panther = Movie.all[1]
+knight = Movie.all[3]
+
+joker.comments.new(user_id: 1, content: "Loved it!")
+joker.comments.new(user_id: 2, content: "Awesome!")
+joker.comments.new(user_id: 3, content: "Meh")
+joker.comments.new(user_id: 4, content: "Disturbing")
+joker.save
+
+panther.comments.new(user_id: 1, content: "What we needed")
+panther.comments.new(user_id: 2, content: "For the culture")
+panther.comments.new(user_id: 3, content: "My favorite!")
+panther.comments.new(user_id: 4, content: "Nice!")
+panther.save
+
+knight.comments.new(user_id: 1, content: "Best EVER")
+knight.comments.new(user_id: 2, content: "Enjoyed it")
+knight.comments.new(user_id: 3, content: "Batman is cool!")
+knight.comments.new(user_id: 4, content: "I liked it")
+knight.save
+
+
+
+
+
+
+
+
+
+
