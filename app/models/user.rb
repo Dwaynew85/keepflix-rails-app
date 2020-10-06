593496898ca1a46901f2f4675f29e79e75c47e1b
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-10-06 13:58:35 -0400

    added default class attribute for pic url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    # class_attribute :pic_url, default: 'https://image.freepik.com/free-photo/black-shade-anatomy-blue-wireframe-face-front-view-dark-background_48049-5.jpg'
     has_secure_password
     has_many :movies
     has_many :comments, through: :movies
