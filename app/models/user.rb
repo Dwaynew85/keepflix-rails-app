d3e9c3ecf07e1c1d88702cea9370bb9d643ad30d
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-10-09 02:42:35 -0400

    added default pic_url method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,16 @@ class User < ApplicationRecord
     validates :name, :email, presence: {message: "Email required"}
     validates :email, uniqueness: {message: "User email in use. Please log in with email or use another."}
 
+    after_initialize :default
+
+    def default
+        if !self.pic_url?
+            self.pic_url = 'https://image.freepik.com/free-photo/black-shade-anatomy-blue-wireframe-face-front-view-dark-background_48049-5.jpg'
+        end
+    end
+
+
+
     def self.find_or_create_by_omniauth(auth_hash)
         self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
             user.password = SecureRandom.hex
