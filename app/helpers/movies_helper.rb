470a2b32c45a329ec243d393886bdc15eb764ec7
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-10-13 14:01:06 -0400

    added helpers and partial to simply views

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,15 @@
 module MoviesHelper
+
+    def trailer_link(movie)
+        if movie.trailer_link != "No Trailer Available"
+            tag.li link_to 'View Trailer', movie.trailer_link, target: '_blank'
+        end
+    end
+
+    def comment_form(movie, comment)
+        form_with(model: [movie, comment], local: true) do |f|
+            f.text_field :content, :placeholder =>"Add Comment"
+            f.submit :style => "display: none"
+        end
+    end
 end
