8787dc1e84893baae20a0b5384b9c337589b3806
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-10-17 01:22:52 -0400

    added my_time method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
     belongs_to :movie
     belongs_to :user 
     validates :content, :presence => true
+
+    def my_time
+        self.updated_at.strftime("%a, %b %d %Y %I:%M%p")
+    end
 end
